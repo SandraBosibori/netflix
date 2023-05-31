e8252fd3c0b0696a5d0779d922fed41860002a77
SandraBosibori <sandrahbosibori@gmail.com>
2023-05-31 10:51:06 +0300

    new netflix changes

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const parent = document.querySelector('.class');
+const parent=document.querySelector('.faq')
 
-// Add event listener to the parent list element
-parent.addEventListener('click', function(event) {
-  // Check if the clicked element is a list item
-  if (event.target.tagName === 'LI') {
-    // Toggle the visibility of the sub-list by adding/removing the 'hidden' class
-    const child = event.target.querySelector('.child');
-    child.classList.toggle('hidden');
-  }
-});
+parent.addEventListener('click', function(e)
+{
+  const child= e.target.querySelector('.child')
+if (e.target.tagName==='DIV') {
+    child.classList.toggle('hidden')
+}})
